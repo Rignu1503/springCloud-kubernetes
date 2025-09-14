d7e6f6652bd5bee92423d2da528dea0a5979c108
Rignu1503 <rigoberto.miranda1503@gmail.com>
2025-09-13 19:27:44 -0500

    add spring cloud

--- msvc-users/src/main/java/dev/rigom/springcloud/msvc/users/MsvcUsersApplication.java
+++ msvc-users/src/main/java/dev/rigom/springcloud/msvc/users/MsvcUsersApplication.java
@@ -2,8 +2,10 @@ package dev.rigom.springcloud.msvc.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient // Habilita el cliente de descubrimiento de servicios (Eureka, Consul, etc.)
 @EnableFeignClients
 @SpringBootApplication
 public class MsvcUsersApplication {
